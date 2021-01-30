627101b54930dc62821dfbce54ff364f740b0c5c
Patrik Drhlík <patrioscraper@gmail.com>
2021-01-30 22:32:30 +0100

    Add typescript definitions
    It includes typescript types and an empty web implementation.

---
+++ src/definitions.ts
@@ -0,0 +1,68 @@
+declare module '@capacitor/core' {
+  interface PluginRegistry {
+    BackgroundGeolocation: BackgroundGeolocationPlugin;
+  }
+}
+
+export interface BackgroundGeolocationOptions {
+  /**
+   * On Android, the plugin shows a notification which allows it to
+   * continue receiving location updates in the background. If this option
+   * is undefined, the notification is not delivered and hence background
+   * location updates are not guaranteed.
+   */
+  backgroundMessage: string,
+  /**
+   * The title for the notification. Defaults to "Using your location".
+   */
+  backgroundTitle: string,
+  /**
+   * Whether permissions should be requested from the user automatically,
+   * if they are not already granted. Defaults to "true".
+   */
+  requestPermissions: boolean,
+  /**
+   * If "true", stale locations may be delivered while the device
+   * obtains a GPS fix. You are responsible for checking the "time"
+   * property. If "false", locations are guaranteed to be up to date.
+   * Defaults to "false".
+   */
+  stale: boolean,
+  /**
+   * The minimum number of metres between subsequent locations. Defaults to 0.
+   */
+  distanceFilter: number
+}
+
+export interface BackgroundGeolocationPlugin {
+  /**
+   * Starts a background geolocation watcher.
+   * @param options Configuration object of type BackgroundGeolocationOptions.
+   * @param callback Receives locations or errors.
+   * @returns Promise<string> returns watcher id.
+   */
+  addWatcher(
+    options: BackgroundGeolocationOptions,
+    callback: (position: GeolocationPosition, error: any) => void
+  ): Promise<string>;
+  /**
+   * Removes a watcher specified by its id.
+   * @param id Watcher id from the addWatcher method.
+   */
+  removeWatcher(id: string): Promise<void>;
+  /**
+   * Opens location settings.
+   */
+  openSettings(): Promise<void>;
+}
+
+export interface GeolocationPosition {
+  latitude: number;
+  longitude: number;
+  accuracy: number;
+  altitude: number;
+  altitudeAccuracy: number;
+  bearing: number;
+  speed: number;
+  time: number;
+}
---
+++ src/web.ts
@@ -0,0 +1,33 @@
+import { WebPlugin } from '@capacitor/core';
+import { BackgroundGeolocationOptions, BackgroundGeolocationPlugin } from './definitions';
+
+export class BackgroundGeolocationWeb extends WebPlugin implements BackgroundGeolocationPlugin {
+  constructor() {
+    super({
+      name: 'BackgroundGeolocation',
+      platforms: ['web'],
+    });
+  }
+
+  public addWatcher(options: BackgroundGeolocationOptions): Promise<string> {
+    console.log("Web not implemented", options);
+    return Promise.resolve("");
+  }
+
+  public removeWatcher(id: string): Promise<void> {
+    console.log("Web not implemented", id);
+    return Promise.resolve();
+  }
+
+  public openSettings(): Promise<void> {
+    return Promise.resolve();
+  }
+
+}
+
+const BackgroundGeolocation = new BackgroundGeolocationWeb();
+
+export { BackgroundGeolocation };
+
+import { registerWebPlugin } from '@capacitor/core';
+registerWebPlugin(BackgroundGeolocation);
