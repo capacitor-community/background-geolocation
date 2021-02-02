cb2c07f786106af617752c61df18045ccea3f2b6
James Diacono <james@diacono.com.au>
2021-02-02 10:51:29 +0930

    Typescript definitions
    close #2

---
+++ definitions.d.ts
@@ -0,0 +1,44 @@
+import {CallbackID} from "@capacitor/core/dist/esm/core-plugin-definitions";
+
+declare module "@capacitor/core" {
+    interface PluginRegistry {
+        BackgroundGeolocation: BackgroundGeolocation;
+    }
+}
+
+export interface WatcherOptions {
+    backgroundMessage?: string;
+    backgroundTitle?: string;
+    requestPermissions?: boolean;
+    stale?: boolean;
+    distanceFilter?: number;
+}
+
+export interface Location {
+    latitude: number;
+    longitude: number;
+    accuracy: number;
+    altitude: number | null;
+    altitudeAccuracy: number | null;
+    bearing: number | null;
+    speed: number | null;
+    time: number | null;
+}
+
+export interface CallbackError extends Error {
+    code?: string;
+}
+
+export interface BackgroundGeolocation {
+    addWatcher(
+        options: WatcherOptions,
+        callback: (
+            position?: Location,
+            error?: CallbackError
+        ) => void
+    ): CallbackID;
+    removeWatcher(options: {
+        id: CallbackID
+    }): Promise<void>;
+    openSettings(): Promise<void>;
+}
